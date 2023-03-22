667171e4e3671ecf44b76a696d60f567b6b077cf
sarbjot <sarbjots908@gmail.com>
2023-03-22 16:24:45 +0530

    connected to db and made deploy ready

--- server/server.js
+++ server/server.js
@@ -4,30 +4,28 @@ require("dotenv").config();
 const connection = require("./db");
 const FraazoController = require("./controller/FraazoController");
 const CartController = require("./controller/CartController");
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
 app.use("/fraazo", FraazoController);
 app.use("/cartItems", CartController);
 
+app.get("/", (req, res) => {
+  // console.log("after fraazo controller");
+  res.send("Welcome to fraazo backend home page");
+});
 
-app.get("/",(req,res)=>{
-    // console.log("after fraazo controller");
-    res.send("Welcome to fraazo backend home page")
-})
+app.listen(PORT, async () => {
+  try {
+    await connection;
+  } catch (err) {
+    console.log(err);
+    console.log("Something went wrong while connecting to db");
+  }
 
-app.listen(PORT, async ()=>{
-    try{
-        await connection;
-    }catch{
-        console.log("Something went wrong while connecting to db");
-    }
-
-    console.log(`Server  listening on port ${PORT}`);
+  console.log(`Server  listening on port ${PORT}`);
 });
